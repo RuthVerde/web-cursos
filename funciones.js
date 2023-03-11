e0a0118d59f0fb0d80a59f1ccb8addba598e5ec8
RuthVerde <114273371+RuthVerde@users.noreply.github.com>
2023-03-11 17:28:09 -0600

    Primer pagina

---
+++ funciones.js
@@ -0,0 +1,23 @@
+function león(){
+location.href = "leo.html"
+}
+
+function hiena(){
+    location.href = "hie.html"
+}
+
+function lobo(){
+    location.href = "lob.html"
+}
+
+function cocodrilo(){
+    location.href = "coco.html"
+}
+
+function pantera(){
+    location.href = "pantera.html"
+}
+
+function redirigir(){
+    location.href = "menuAnimales.html"
+}
